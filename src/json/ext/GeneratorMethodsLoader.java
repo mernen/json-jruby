467f5f4f3aba4cbb8fe8722df6f97639ec389181
mernen <mernen@33969fd8-97b7-4be6-959d-88e207f89eea>
2008-01-27 22:51:54 +0000

    Fixed bug in escapeUnicode
    git-svn-id: svn+ssh://rubyforge.org/var/svn/json-jruby/trunk@27 33969fd8-97b7-4be6-959d-88e207f89eea

--- src/json/ext/GeneratorMethodsLoader.java
+++ src/json/ext/GeneratorMethodsLoader.java
@@ -107,8 +107,8 @@ class GeneratorMethodsLoader {
 			final byte[] hex = new byte[] {'0', '1', '2', '3', '4', '5', '6', '7',
 			                               '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 			return new byte[] {
-				'\\', 'u', hex[(c >>> 12) & 16], hex[(c >>> 8) & 16],
-				           hex[(c >>>  4) & 16], hex[c & 16]};
+				'\\', 'u', hex[(c >>> 12) % 16], hex[(c >>> 8) % 16],
+				           hex[(c >>>  4) % 16], hex[c % 16]};
 		}
 	};
 
