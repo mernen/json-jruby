7a953f5028459edcab3e410d7af531a93309a910
mernen <mernen@33969fd8-97b7-4be6-959d-88e207f89eea>
2008-01-29 03:51:53 +0000

    Changed "C" references on lib/json/ext.rb into "Java" :)
    git-svn-id: svn+ssh://rubyforge.org/var/svn/json-jruby/trunk@39 33969fd8-97b7-4be6-959d-88e207f89eea

--- lib/json/ext.rb
+++ lib/json/ext.rb
@@ -2,11 +2,11 @@ require 'json/common'
 
 module JSON
   # This module holds all the modules/classes that implement JSON's
-  # functionality as C extensions.
+  # functionality as Java extensions.
   module Ext
     require 'json/ext/parser'
     require 'json/ext/generator'
-    $DEBUG and warn "Using c extension for JSON."
+    $DEBUG and warn "Using Java extension for JSON."
     JSON.parser = Parser
     JSON.generator = Generator
   end
